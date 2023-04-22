8cc9d815e56cd7d5c6ce044e20956fa38f3b3cc5
Ravipatel1309 <rpatel8511333@gmail.com>
2023-04-22 17:37:24 +0530

    pushing backend

--- server.js
+++ server.js
@@ -30,13 +30,12 @@ app.use('/api/users', userRoutes);
 app.use('/api/notes', noteRoutes);
 
 // --------------------------deployment------------------------------
-__dirname = path.resolve();
+// __dirname = 'http://localhost:5000';
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/frontend/build")));
-
+    app.use(express.static(path.join(__dirname, "../frontend/build")));
     app.get("*", (req, res) =>
-        res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+        res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"))
     );
 } else {
     app.get("/", (req, res) => {
